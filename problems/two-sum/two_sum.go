f684ed38d833ada2c18c872a0b00e23a0bb19927
Karuppiah Natarajan <karuppiah7890@gmail.com>
2023-09-14 17:53:12 +0530

    add basic quick implementation with a failing test

--- problems/two-sum/two_sum.go
+++ problems/two-sum/two_sum.go
@@ -1,11 +1,37 @@
 package twosum
 
 func TwoSum(nums []int, target int) []int {
-	return TwoSumV1(nums, target)
+	return TwoSumQuick(nums, target)
 }
 
-// Brute Force. O(N^2)
-func TwoSumV1(nums []int, target int) []int {
+// Cases to consider
+// 1. There can be more than one instance / occurance of the same number
+// 2. The target can be 0
+
+// Questions to consider
+// 1. Can the numbers be negative? Yes! -10^9 <= nums[i] <= 10^9
+// 2. Can the target be negative? Yes! -10^9 <= target <= 10^9
+// 3. What's the max number of numbers that can be present in the array? 2 <= nums.length <= 10^4
+
+// Quick. Time = O(N)
+func TwoSumQuick(nums []int, target int) []int {
+	seenSmallNumbers := map[int][]int{}
+
+	for index, num := range nums {
+		if num < target {
+			num2 := target - num
+			num2Indices, ok := seenSmallNumbers[num2]
+			if ok {
+				return []int{index, num2Indices[0]}
+			}
+			seenSmallNumbers[num] = append(seenSmallNumbers[num], index)
+		}
+	}
+	return nil
+}
+
+// Brute Force. Time = O(N^2)
+func TwoSumBruteForce(nums []int, target int) []int {
 	for index, num := range nums {
 		for j := index + 1; j < len(nums); j++ {
 			otherNumber := nums[j]
--- problems/two-sum/two_sum_test.go
+++ problems/two-sum/two_sum_test.go
@@ -18,10 +18,23 @@ func TestTwoSum(t *testing.T) {
 		{nums: []int{2, 7, 11, 15}, target: 9, expectedOutput: []int{0, 1}},
 		{nums: []int{3, 2, 4}, target: 6, expectedOutput: []int{1, 2}},
 		{nums: []int{3, 3}, target: 6, expectedOutput: []int{0, 1}},
+		{nums: []int{0, 4, 3, 0}, target: 0, expectedOutput: []int{0, 3}},
 	}
 
 	for _, testCase := range testCases {
 		output := twosum.TwoSum(testCase.nums, testCase.target)
 		assert.ElementsMatch(t, testCase.expectedOutput, output)
+	}
+}
+
+func BenchmarkTwoSumBruteForce(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		twosum.TwoSumBruteForce([]int{2, 7, 11, 15}, 9)
+	}
+}
+
+func BenchmarkTwoSumQuick(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		twosum.TwoSumQuick([]int{2, 7, 11, 15}, 9)
 	}
 }
