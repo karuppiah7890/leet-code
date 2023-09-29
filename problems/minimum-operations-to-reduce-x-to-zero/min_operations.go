a9abd086e933094fc6627e67b26f0209451ca02b
Karuppiah Natarajan <karuppiah7890@gmail.com>
2023-09-29 21:59:52 +0530

    implement minimum operations to reduce x to 0 in a non optimized manner
    added a big test case which checks for optmization - speed. test case
    leads to test timeout - it runs for beyond 30 seconds

--- problems/minimum-operations-to-reduce-x-to-zero/min_operations.go
+++ problems/minimum-operations-to-reduce-x-to-zero/min_operations.go
@@ -1,6 +1,11 @@
 package min_operations
 
 func MinOperations(nums []int, x int) int {
+	// array of size 0
+	if len(nums) == 0 {
+		return -1
+	}
+
 	// array of size 1
 	if len(nums) == 1 {
 		if nums[0] == x {
@@ -8,34 +13,56 @@ func MinOperations(nums []int, x int) int {
 			return 1
 		} else {
 			// here (nums[0] > x) or (nums[0] < x).
-			// hence no operations can be subtracted
-			// from x to make it 0
-			return -1
-		}
-	}
-	if len(nums) > 1 {
-		if nums[0] == x || nums[len(nums)-1] == x {
-			// the first or last number could be the same as x.
-			// or both the first and last number could be the same as x.
-			// In both cases, just 1 operation to subtract the single number from x.
-			return 1
-		}
-		if nums[0] > x && nums[len(nums)-1] > x {
+			// hence no operations can be done to
+			// make x as 0 by subtracting num[0] from it
 			return -1
 		}
 	}
 
-	// remove leftmost number and follow that path and try to make x as 0
+	first := nums[0]
+	last := nums[len(nums)-1]
 
-	// remove rightmost number and follow that path and try to make x as 0
+	if first == x || last == x {
+		// the first or last number could be the same as x.
+		// or both the first and last number could be the same as x.
+		// In both cases, just 1 operation to subtract the single number from x.
+		return 1
+	}
+	if first > x && last > x {
+		return -1
+	}
 
-	// if any one - leftmost or rightmost number and
+	// the first and/ the last number is less than x
+
+	// remove first number and follow that path and try to make x as 0
+	numberOfOperationsUsingFirstNumber := MinOperations(nums[1:], x-first)
+
+	// remove last number and follow that path and try to make x as 0
+	numberOfOperationsUsingLastNumber := MinOperations(nums[:len(nums)-1], x-last)
+
+	// if both first and last number removal path does not
+	// lead x to become 0 on subtracting the numbers, then
+	// return -1
+	if numberOfOperationsUsingFirstNumber == -1 && numberOfOperationsUsingLastNumber == -1 {
+		return -1
+	}
+
+	// if any one - first or last number, along with
 	// more numbers can be subtracted from x to make it 0
 	// then return the number of operations.
-	// ELSE
-	// if both leftmost and rightmost number path does not
-	// lead x to become 0 on subtracting numbers, then
-	// return -1
+	if numberOfOperationsUsingFirstNumber == -1 {
+		return numberOfOperationsUsingLastNumber + 1
+	}
+	if numberOfOperationsUsingLastNumber == -1 {
+		return numberOfOperationsUsingFirstNumber + 1
+	}
 
-	return 0
+	// if both first number and last number path can be used to
+	// make x as 0, then return the minimum of the two
+	// Note: Below logic works even if both the values are equal, which is
+	// also possible
+	if numberOfOperationsUsingFirstNumber < numberOfOperationsUsingLastNumber {
+		return numberOfOperationsUsingFirstNumber + 1
+	}
+	return numberOfOperationsUsingLastNumber + 1
 }
